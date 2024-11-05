7cb12ab29b67139e799b7217304c077b034622e8
Petecariu Nicoleta <nicoletaflorina020@gmail.com>
2024-11-05 13:31:52 +0000

    medical analisys heatmap/categorical plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -11,25 +11,27 @@ df['overweight'] = None
 
 # 3 Normalize data by making 0 always good and 1 always bad. If the value of cholesterol or gluc is 1, set the value to 0. 
 #If the value is more than 1, set the value to 1.
+# Set values to 0 where cholesterol or gluc is 1
+df.loc[df['cholesterol'] == 1, 'cholesterol'] = 0
+df.loc[df['gluc'] == 1, 'gluc'] = 0
 
+# Set values to 1 where cholesterol or gluc is greater than 1
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 # 4Draw the Categorical Plot in the draw_cat_plot function
 def draw_cat_plot():
-    # 5
-    df_cat = None
-
-
-    # 6
-    df_cat = None
+   # 5
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
-
+    # 6
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'])['value'].count().reset_index(name="total")
+    
     # 7
-
-
+    sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat)
 
     # 8
-    fig = None
-
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat)
 
     # 9
     fig.savefig('catplot.png')
@@ -39,23 +41,25 @@ def draw_cat_plot():
 # 10 Draw the Heat Map in the draw_heat_map function
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
+    mask = np.triu(corr)
 
-
-
-    # 14
-    fig, ax = None
+    # 14 Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
-
-
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt='.1f')
     # 16
     fig.savefig('heatmap.png')
     return fig
